7ed47d6fde3a5729ad19b296eacae854910db9a2
Tejus Gupta <tejus3131@gmail.com>
2023-07-29 03:42:24 +0530

    initial commit

---
+++ app.py
@@ -0,0 +1,23 @@
+from SparkleWeb import Server
+from flask import Flask
+
+base = {
+    "template": "index.html",
+    "title": "Index",
+    "args": {},
+    "reloadRequired": []
+}
+
+cssDict = {
+    "main": "http://127.0.0.1:5300/static/main.css"
+}
+
+jsDict = {
+    "main": "http://127.0.0.1:5300/static/main.js"
+}
+
+flask_app = Flask(__name__)
+app = Server(flask_app, base, cssDict, jsDict)
+
+if __name__ == "__main__":
+    app.run()
