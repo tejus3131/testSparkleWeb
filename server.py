bb00209c5244465c946488e19c6fb392ca722a49
Tejus Gupta <tejus3131@gmail.com>
2023-08-01 10:14:39 +0530

    minor fix

--- server.py
+++ server.py
@@ -1,6 +1,7 @@
-from SparkleWeb import Server
+import SparkleWeb
+from datetime import datetime
 
-server = Server()
+server = SparkleWeb.Server()
 
 server.setCss({
     "customCSS":"http://127.0.0.1:5300/static/custom.css",
@@ -12,7 +13,10 @@ server.setJs({
     "bootstrap":"https://cdn.jsdelivr.net/npm/bootstrap@5.3.1/dist/js/bootstrap.bundle.min.js"
     })
 
-server.setIndex("index.html")
+server.setIndex("index.html", 
+                "SparkleWeb",
+                {"time":datetime.now().isoformat()},
+                ["customJS"])
 
 if __name__ == "__main__":
     server.run(debug=True)
---
+++ static/custom.js
@@ -0,0 +1,1 @@
+console.log("customJS Active")
